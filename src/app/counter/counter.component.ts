d9aabf87345d1243a8afca684d7bf32c9e2fedc1
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-26 21:28:12 -0400

    Contador Component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// @Component() es un decorador que transforma mi clase en un componente
+@Component({
+  selector: 'app-counter', // => nombre de la etiqueta
+  template: '<h1>Hola Counter</h1>', // => template en línea del HTML
+
+})
+export class CounterComponent {
+
+
+
+}
