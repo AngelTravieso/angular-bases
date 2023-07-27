916c3e73327060fd69c8b5a91b9abfb8ce1b1826
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-27 18:46:14 -0400

    Componente Hero y directorios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './hero/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Los componentes creados deben estar aquí declarado
     CounterComponent,
+    HeroComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule
