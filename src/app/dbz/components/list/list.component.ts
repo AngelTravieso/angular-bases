3f3e38f5ea7dc3b0fd982e302def0be662554739
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-28 19:02:02 -0400

    @Input  Recibir del componente padre

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
+
+  /**
+   * Con este decorador decimos que mi ListComponent puede recibir una property llamada `characterList`,
+   * es decir, @Input define una propiedad que puede ser enviada desde un componente padre al componente hijo
+   */
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10,
+  }];
 
 }
